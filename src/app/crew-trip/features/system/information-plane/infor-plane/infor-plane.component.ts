c89b10bf044399b2b8f2a7fe923cd08dc111693e
dxhop <dxhop@gimasys.com>
2024-11-06 14:30:51 +0700

    Add InfoPlaneService and refactor infor-plane component
    Introduced InfoPlaneService for plane-related operations. Refactored infor-plane component HTML and TypeScript to improve data binding and functionality, including removing unused code and aligning with CommonComponent structure.

--- src/app/crew-trip/features/system/information-plane/infor-plane/infor-plane.component.ts
+++ src/app/crew-trip/features/system/information-plane/infor-plane/infor-plane.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +10,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core'; // Import MatOptionModule nếu cần thiết
 import { MatIconModule } from '@angular/material/icon';
 import {InputSizeComponent} from "src/app/crew-trip/shared/input/input-size.component";
+import {CommonComponent} from "src/app/crew-trip/shared/common.component";
+import {InfoPlaneService} from "src/app/crew-trip/core/services/InfoPlaneService.service";
+
 @Component({
   selector: 'app-infor-plane',
   standalone: true,
@@ -25,67 +28,72 @@ import {InputSizeComponent} from "src/app/crew-trip/shared/input/input-size.comp
   templateUrl: './infor-plane.component.html',
   styleUrls: ['./infor-plane.component.scss']
 })
-export class InforPlaneComponent {
-  displayedColumns: string[] = ['index', 'acGroup', 'acType', 'note', 'status', 'action'];
-  infor = [
-    { acGroup: '320', acType: '32L', note: '', status: 'Active' },
-    { acGroup: '321', acType: '32A', note: '', status: 'Active' },
-    { acGroup: '350', acType: '35A', note: '', status: 'Active' },
-    { acGroup: '787', acType: '78A', note: '', status: 'Active' },
-    { acGroup: 'ART', acType: 'AT7', note: '', status: 'Active' },
-  ];
-  formGroupSearch: FormGroup;
-  dataSource: any[] = []; // Đây là dữ liệu mẫu, bạn có thể cập nhật theo nhu cầu của mình
+export class InforPlaneComponent extends CommonComponent {
+  override baseService = inject(InfoPlaneService);
+  override displayedColumns: string[] = ['index', 'acGroup', 'acType', 'note', 'status', 'action'];
+  // infor = [
+  //   { acGroup: '320', acType: '32L', note: '', status: 'Active' },
+  //   { acGroup: '321', acType: '32A', note: '', status: 'Active' },
+  //   { acGroup: '350', acType: '35A', note: '', status: 'Active' },
+  //   { acGroup: '787', acType: '78A', note: '', status: 'Active' },
+  //   { acGroup: 'ART', acType: 'AT7', note: '', status: 'Active' },
+  // ];
+  override formGroupSearch: FormGroup;
+  // override dataSource: any[] = []; // Đây là dữ liệu mẫu, bạn có thể cập nhật theo nhu cầu của mình
 
-  totalElement = 0;
-  pageSize = 10;
-  pageSizeOptions = [5, 10, 25, 50];
-  pageIndex = 0;
-  showFirstLastButtons = true;
-  showDialogDelete = false;
+  override totalElement = 0;
+  override pageSize = 10;
+  override pageSizeOptions = [5, 10, 25, 50];
+  override pageIndex = 0;
+  override showFirstLastButtons = true;
+  override showDialogDelete = false;
   statuses: string[] = ['Active', 'Inactive', 'Pending'];
   constructor(private fb: FormBuilder) {
+    super();
     this.formGroupSearch = this.fb.group({
-      s: [''],status: ['']
+      s: ['',], active: ['',], area: ['',],
     });
+    this.formGroupSearchInit = {...this.formGroupSearch.value}
   }
 
 
-  async ngOnInit() {
+  override async ngOnInit() {
     //call api
     await Promise.all([this.search(),]).then(() => {
       console.log(this.dataSource)
     });
   }
+  createAirplane() {
 
-  search() {
-    // Thêm logic tìm kiếm tại đây
-    console.log('Search clicked', this.formGroupSearch.value);
   }
-
-  grMailDetail(id?: number, mode?: string) {
-    // Logic hiển thị chi tiết group mail
-    console.log('Detail clicked', { id, mode });
-  }
-
-  showConfirmDelete(id: number) {
-    this.showDialogDelete = true;
-    console.log('Show confirm delete dialog for ID:', id);
-  }
-
-  toggleDialogDelete() {
-    this.showDialogDelete = !this.showDialogDelete;
-  }
-
-  delete() {
-    // Logic xoá phần tử
-    console.log('Delete action confirmed');
-    this.showDialogDelete = false;
-  }
-
-  onPageChange(event: any) {
-    console.log('Page change event:', event);
-    // Cập nhật dữ liệu dựa vào phân trang
-  }
+  // search() {
+  //   // Thêm logic tìm kiếm tại đây
+  //   console.log('Search clicked', this.formGroupSearch.value);
+  // }
+  //
+  // grMailDetail(id?: number, mode?: string) {
+  //   // Logic hiển thị chi tiết group mail
+  //   console.log('Detail clicked', { id, mode });
+  // }
+  //
+  // showConfirmDelete(id: number) {
+  //   this.showDialogDelete = true;
+  //   console.log('Show confirm delete dialog for ID:', id);
+  // }
+  //
+  // toggleDialogDelete() {
+  //   this.showDialogDelete = !this.showDialogDelete;
+  // }
+  //
+  // delete() {
+  //   // Logic xoá phần tử
+  //   console.log('Delete action confirmed');
+  //   this.showDialogDelete = false;
+  // }
+  //
+  // onPageChange(event: any) {
+  //   console.log('Page change event:', event);
+  //   // Cập nhật dữ liệu dựa vào phân trang
+  // }
 
 }
