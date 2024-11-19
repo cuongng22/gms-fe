96372ff7fdb28633b6c4f65deb8edc1887723b05
TheHoa <thehoa268@gmail.com>
2024-11-19 09:17:20 +0700

    update menu

--- src/app/crew-trip/features/menu/sidebar/sidebar.model.ts
+++ src/app/crew-trip/features/menu/sidebar/sidebar.model.ts
@@ -43,7 +43,7 @@ export const menu: Menu[] = [
         icon: 'ri-exchange-cny-line'
       },
       {
-        path: 'category/crews', title: $localize`:@@flightCrew:Flight Crew`, icon: 'ri-id-card-line'
+        path: 'category/crews-flight', title: $localize`:@@flightCrew:Flight Crew`, icon: 'ri-id-card-line'
       },
       {
         path: 'category/crews', title: $localize`:@@crewList:Crew List`, icon: 'ri-id-card-line'
