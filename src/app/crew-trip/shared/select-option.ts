7163ed8743a584204467c0fd3d0a98e7e0b459d2
TheHoa <thehoa268@gmail.com>
2024-11-22 09:04:51 +0700

    update cpn select

--- src/app/crew-trip/shared/select-option.ts
+++ src/app/crew-trip/shared/select-option.ts
@@ -5,7 +5,6 @@ export const SelectOptions = {
     {value: 0, display: 'Female'},
     {value: 1, display: 'Male'},
   ],
-  // Add more options here
   STATUS: [
     {value: null, display: 'Select status'},
     {value: 'active', display: 'Active'},
