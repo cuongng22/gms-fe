fbad3afa98fbf6a1bc6db3ca9145be59bfbada59
TheHoa <thehoa268@gmail.com>
2024-12-10 10:37:46 +0700

    fix bug b10

--- src/app/crew-trip/features/category/flight-crew/config-overnight-rate/config-overnight-rate.component.ts
+++ src/app/crew-trip/features/category/flight-crew/config-overnight-rate/config-overnight-rate.component.ts
@@ -2,19 +2,27 @@ import {Component, ElementRef, inject, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {InputSizeComponent} from "src/app/crew-trip/shared/input/input-size.component";
 import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
-import {MatAnchor, MatButton} from "@angular/material/button";
+import {MatAnchor, MatButton, MatButtonModule} from "@angular/material/button";
 import {
   MatCard,
   MatCardActions,
   MatCardContent,
-  MatCardHeader,
+  MatCardHeader, MatCardModule,
   MatCardSubtitle,
   MatCardTitle
 } from "@angular/material/card";
-import {MatError, MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {
+  MatError,
+  MatFormField,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatSelect} from "@angular/material/select";
-import {DecimalPipe, NgClass, NgForOf} from "@angular/common";
+import {CommonModule, DecimalPipe, NgClass, NgForOf, NgIf, TitleCasePipe} from "@angular/common";
 import {SelectMultipleComponent} from "src/app/crew-trip/shared/component/select-multiple/select-multiple.component";
 import {CommonComponent} from "src/app/crew-trip/shared/common.component";
 import {FlightCrewService} from "src/app/crew-trip/core/services/flight-crew-service";
@@ -30,51 +38,27 @@ import {
   MatHeaderCell,
   MatHeaderRow,
   MatHeaderRowDef,
-  MatRow, MatRowDef, MatTable
+  MatRow, MatRowDef, MatTable, MatTableModule
 } from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
 import {FileUploadComponent, FileUploadValidators} from "@iplab/ngx-file-upload";
 import {MESSAGE} from "src/app/crew-trip/shared/utils/constant";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {DataTransformPipe} from "src/app/crew-trip/shared/data-transform.pipe";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {RoleFunctionComponent} from "src/app/crew-trip/features/roles/role-function/role-function.component";
+import {NoDataRowOutlet} from "@angular/cdk/table";
+import {InputComponent} from "src/app/ui-elements/input/input.component";
+import {NgxTrimDirectiveModule} from "ngx-trim-directive";
+import {OtherCrewComponent} from "src/app/crew-trip/features/category/flight-crew/other-crew/other-crew.component";
+import {SelectionComponent} from "src/app/crew-trip/shared/component/selection/selection.component";
 
 @Component({
   selector: 'app-config-overnight-rate',
   standalone: true,
   imports: [
-    FormsModule,
-    InputSizeComponent,
-    MatAutocomplete,
-    MatAutocompleteTrigger,
-    MatButton,
-    MatCard,
-    MatCardContent,
-    MatCardHeader,
-    MatCardTitle,
-    MatFormField,
-    MatInput,
-    MatLabel,
-    MatOption,
-    MatSelect,
-    NgForOf,
-    ReactiveFormsModule,
-    SelectMultipleComponent,
-    DecimalPipe,
-    MatAnchor,
-    MatCardSubtitle,
-    MatCell,
-    MatCellDef,
-    MatColumnDef,
-    MatHeaderCell,
-    MatHeaderRow,
-    MatHeaderRowDef,
-    MatPaginator,
-    MatRow,
-    MatRowDef,
-    MatTable,
-    FileUploadComponent,
-    MatCardActions,
-    MatError,
-    MatHint,
-    NgClass
+    CommonModule, MatCardModule, MatFormFieldModule, MatButtonModule, MatMenuModule, MatTableModule, MatPaginatorModule, NgIf, MatCheckboxModule, TitleCasePipe, DataTransformPipe, NgClass, MatFormField, MatSelect, MatOption, MatInput, MatLabel, ReactiveFormsModule, MatError, MatPrefix, MatSuffix, MatTab, MatTabGroup, RoleFunctionComponent, NoDataRowOutlet, InputComponent, NgxTrimDirectiveModule, OtherCrewComponent, ConfigOvernightRateComponent, MatAutocomplete, MatAutocompleteTrigger, SelectionComponent, InputSizeComponent, SelectMultipleComponent, FileUploadComponent
   ],
   templateUrl: './config-overnight-rate.component.html',
   styleUrl: './config-overnight-rate.component.scss'
@@ -102,12 +86,12 @@ export class ConfigOvernightRateComponent extends CommonComponent implements OnI
 
   override async ngOnInit() {
     super.ngOnInit();
-    this.displayedColumns = ['stt', 'market', 'acType', 'pilotNumber', 'attendantNumber', 'remark', 'status', 'action'];
     await Promise.all([
       this.getListAirport(),
       this.search(),
     ]).then(() => {
     });
+    this.displayedColumns = ['stt', 'marketCode', 'nightCount', 'rate', 'remark', 'action'];
   }
 
 
