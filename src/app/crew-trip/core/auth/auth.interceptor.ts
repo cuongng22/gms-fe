0a75435de579f4d59a51f094dcf0abb4cfa8f569
TheHoa <thehoa268@gmail.com>
2024-12-25 14:29:25 +0700

    update timeout call api /api/productivity

--- src/app/crew-trip/core/auth/auth.interceptor.ts
+++ src/app/crew-trip/core/auth/auth.interceptor.ts
@@ -7,17 +7,17 @@ import {
   HttpRequest,
   HttpStatusCode
 } from '@angular/common/http';
-import { from, Observable, tap, throwError, timeout } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { BaseService } from '../services/base-service';
-import { MESSAGE, COMMON_CONFIG } from '../../shared/utils/constant';
-import { STORAGE_KEY } from 'src/app/crew-trip/core/constants/config';
-import { LanguageService } from 'src/app/crew-trip/core/services/language.service';
-import { UsersService } from "src/app/crew-trip/core/services/users-service";
-import { MIMEType } from 'util';
-import { el } from 'node_modules/@fullcalendar/core/internal-common';
+import {from, Observable, tap, throwError, timeout} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {BaseService} from '../services/base-service';
+import {MESSAGE, COMMON_CONFIG} from '../../shared/utils/constant';
+import {STORAGE_KEY} from 'src/app/crew-trip/core/constants/config';
+import {LanguageService} from 'src/app/crew-trip/core/services/language.service';
+import {UsersService} from "src/app/crew-trip/core/services/users-service";
+import {MIMEType} from 'util';
+import {el} from 'node_modules/@fullcalendar/core/internal-common';
 
 
 export function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
@@ -33,8 +33,12 @@ export function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerF
   if (token) {
     headers = headers.set('Authorization', `Bearer ${token}`);
   }
-  const authReq = req.clone({ headers });
-  return next(authReq).pipe(timeout(COMMON_CONFIG.TIMEOUT), tap(event => {
+  let requestTimeout = COMMON_CONFIG.TIMEOUT;
+  if (req.url.includes('/api/productivity')) {
+    requestTimeout = 1000000; // Timeout 10 giây cho đường dẫn cụ thể
+  }
+  const authReq = req.clone({headers});
+  return next(authReq).pipe(timeout(requestTimeout), tap(event => {
     if (event.type === HttpEventType.Response) {
       // console.log(req.url, 'returned a response with status', event.status);
     }
@@ -49,7 +53,7 @@ export function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerF
       readError(error).then((errorResponse: HttpErrorResponse) => {
         if (errorResponse.status === HttpStatusCode.Unauthorized) {
           usersService.logout();
-          router.navigate(['auth/login'], { fragment: HttpStatusCode.Unauthorized.toString(), skipLocationChange: true });
+          router.navigate(['auth/login'], {fragment: HttpStatusCode.Unauthorized.toString(), skipLocationChange: true});
         } else if (errorResponse.status === HttpStatusCode.ServiceUnavailable || (errorResponse.status === 0 && errorResponse.statusText === 'Unknown Error')) {
           baseService.showError(MESSAGE.ERROR_CONNECT);
         } else if (errorResponse.status === HttpStatusCode.BadRequest) {
