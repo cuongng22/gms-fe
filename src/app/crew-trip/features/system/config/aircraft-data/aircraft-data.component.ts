8a9c137d676f7af83a1e0bdf54569c2db9ab9a8b
dxhop <dxhop@gimasys.com>
2024-12-26 17:20:32 +0700

    fix bug màn email supplier

--- src/app/crew-trip/features/system/config/aircraft-data/aircraft-data.component.ts
+++ src/app/crew-trip/features/system/config/aircraft-data/aircraft-data.component.ts
@@ -1,61 +1,45 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {DataTransformPipe} from "src/app/crew-trip/shared/data-transform.pipe";
 import {
   AbstractControl,
   FormBuilder,
-  FormsModule,
   ReactiveFormsModule,
   ValidationErrors,
   Validators
-} from "@angular/forms";
-import {InputSizeComponent} from "src/app/crew-trip/shared/input/input-size.component";
-import {MatAnchor, MatButton, MatButtonModule} from "@angular/material/button";
+} from '@angular/forms';
+import {InputSizeComponent} from 'src/app/crew-trip/shared/input/input-size.component';
+import { MatButtonModule} from '@angular/material/button';
 import {
-  MatCard,
-  MatCardContent,
-  MatCardHeader,
+
   MatCardModule,
-  MatCardSubtitle,
-  MatCardTitle
-} from "@angular/material/card";
+
+} from '@angular/material/card';
 import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderRow,
-  MatHeaderRowDef,
-  MatRow, MatRowDef, MatTable, MatTableModule
-} from "@angular/material/table";
-import {MatCheckbox, MatCheckboxModule} from "@angular/material/checkbox";
-import {MatError, MatFormField, MatHint, MatLabel, MatPrefix, MatSuffix} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
-import {MatOption} from "@angular/material/core";
-import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
-import {MatSelect} from "@angular/material/select";
-import {CommonModule, NgClass, NgIf, TitleCasePipe} from "@angular/common";
-import {NgxTrimDirectiveModule} from "ngx-trim-directive";
-import {CommonComponent} from "src/app/crew-trip/shared/common.component";
-import {NationService} from "src/app/crew-trip/core/services/nation-service";
-import {UsersService} from "src/app/crew-trip/core/services/users-service";
-import {InfoPlaneService} from "src/app/crew-trip/core/services/InfoPlaneService.service";
-import {
-  FlightCrewDetailComponent
-} from "src/app/crew-trip/features/category/flight-crew/flight-crew-detail/flight-crew-detail.component";
-import {RouterLink} from "@angular/router";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {RoleFunctionComponent} from "src/app/crew-trip/features/roles/role-function/role-function.component";
-import {NoDataRowOutlet} from "@angular/cdk/table";
-import {InputComponent} from "src/app/crew-trip/shared/component/input/input.component";
-import {MESSAGE} from "src/app/crew-trip/shared/utils/constant";
-import {HttpStatusCode} from "@angular/common/http";
+  MatTableModule
+} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatError, MatFormField, MatHint, MatLabel, MatPrefix, MatSuffix} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatOption} from '@angular/material/core';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import {MatSelect} from '@angular/material/select';
+import {CommonModule, NgClass, NgIf} from '@angular/common';
+import {NgxTrimDirectiveModule} from 'ngx-trim-directive';
+import {CommonComponent} from 'src/app/crew-trip/shared/common.component';
+import {UsersService} from 'src/app/crew-trip/core/services/users-service';
+import {InfoPlaneService} from 'src/app/crew-trip/core/services/InfoPlaneService.service';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {RoleFunctionComponent} from 'src/app/crew-trip/features/roles/role-function/role-function.component';
+import {NoDataRowOutlet} from '@angular/cdk/table';
+import {InputComponent} from 'src/app/crew-trip/shared/component/input/input.component';
+import {MESSAGE} from 'src/app/crew-trip/shared/utils/constant';
+import {HttpStatusCode} from '@angular/common/http';
 
 @Component({
   selector: 'app-aircraft-data',
   standalone: true,
   imports: [
-    RouterLink, CommonModule, MatCardModule, MatButtonModule, MatMenuModule, MatTableModule, MatPaginatorModule, NgIf, MatCheckboxModule, TitleCasePipe, DataTransformPipe, NgClass, MatFormField, MatSelect, MatOption, MatInput, MatLabel, ReactiveFormsModule, InputSizeComponent, MatError, MatPrefix, MatSuffix, MatTab, MatTabGroup, RoleFunctionComponent, NoDataRowOutlet, InputComponent, NgxTrimDirectiveModule, MatHint
+    CommonModule, MatCardModule, MatButtonModule, MatMenuModule, MatTableModule, MatPaginatorModule, NgIf, MatCheckboxModule, NgClass, MatFormField, MatSelect, MatOption, MatInput, MatLabel, ReactiveFormsModule, InputSizeComponent, MatError, MatPrefix, MatSuffix, MatTab, MatTabGroup, RoleFunctionComponent, NoDataRowOutlet, InputComponent, NgxTrimDirectiveModule, MatHint
   ],
   templateUrl: './aircraft-data.component.html',
   styleUrl: './aircraft-data.component.scss'
@@ -64,7 +48,7 @@ export class AircraftDataComponent extends CommonComponent implements OnInit {
   override baseService = inject(InfoPlaneService);
   usersService = inject(UsersService);
   fb = inject(FormBuilder);
-  existCheck: boolean = false;
+  existCheck = false;
 
 
   constructor() {
@@ -103,7 +87,7 @@ export class AircraftDataComponent extends CommonComponent implements OnInit {
       id: id,
       acType: actype
     });
-    console.log("this.formGroupDetailthis.formGroupDetail:", this.formGroupDetail)
+    console.log('this.formGroupDetailthis.formGroupDetail:', this.formGroupDetail);
     this.toggleDialogDelete();
   }
 
@@ -135,6 +119,6 @@ export class AircraftDataComponent extends CommonComponent implements OnInit {
   }
 
   existValidator(control: AbstractControl): ValidationErrors | null {
-    return this.existCheck ? {existCheck: true} : null
+    return this.existCheck ? {existCheck: true} : null;
   }
 }
