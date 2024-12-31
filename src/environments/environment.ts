e7e7971defaaf532b81bc6a291edd91f8ecef50e
Do Xuan Hop <dxhop@gimasys.com>
2024-12-31 01:44:12 +0000

    feature/update-production-deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://',
-  baseUrl: 'http://',
-  socketUrl: ''
+  apiUrl: 'http://171.254.94.64:8080/api',
+  baseUrl: 'http://171.254.94.64:8080',
+  socketUrl: 'wss://171.254.94.64:8080'
 };
