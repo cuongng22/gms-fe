4f20cd3a184cc4c65808b18f8034ebe6aff81265
TheHoa <thehoa268@gmail.com>
2024-12-31 16:50:18 +0700

    update group mail

---
+++ src/app/crew-trip/core/guards/redirect-guard.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
+import {StorageService} from "src/app/crew-trip/core/services/storage.service";
+import {UsersService} from "src/app/crew-trip/core/services/users-service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedirectGuard implements CanActivate {
+  constructor(private usersService: UsersService, private router: Router) {
+  }
+
+  canActivate(): boolean {
+    if (this.usersService.isLoggedIn()) {
+      this.router.navigate(['/category/vehicle']);
+    } else {
+      this.router.navigate(['/auth/login']);
+    }
+    return false;
+  }
+}
