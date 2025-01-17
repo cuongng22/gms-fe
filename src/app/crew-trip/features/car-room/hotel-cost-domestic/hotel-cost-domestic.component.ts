a45317523dd7fa2cb1cdde0758866b368506c8bd
Do Xuan Hop <dxhop@gimasys.com>
2025-01-17 02:46:20 +0000

    Title: Implement domestic hotel cost tracking and formatting improvements

---
+++ src/app/crew-trip/features/car-room/hotel-cost-domestic/hotel-cost-domestic.component.ts
@@ -0,0 +1,245 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardModule } from '@angular/material/card';
+import { MatOption } from '@angular/material/core';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatSelect } from '@angular/material/select';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatFooterCell,
+  MatFooterCellDef,
+  MatFooterRow,
+  MatFooterRowDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatNoDataRow,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from '@angular/material/table';
+import { AvesCostTrackingService } from 'src/app/crew-trip/core/services/aves-cost-tracking.service';
+import { FlightMarketService } from 'src/app/crew-trip/core/services/flight-market.service';
+import { CommonComponent } from 'src/app/crew-trip/shared/common.component';
+import { SelectionSuggestComponent } from 'src/app/crew-trip/shared/component/selection-suggest/selection-suggest.component';
+import { DataCalculateTotal } from 'src/app/crew-trip/shared/data-calculate-total';
+import { InputSizeComponent } from 'src/app/crew-trip/shared/input/input-size.component';
+
+@Component({
+  selector: 'app-hotel-cost-domestic',
+  standalone: true,
+  imports: [
+    FormsModule,
+    InputSizeComponent,
+    MatButton,
+    MatCard,
+    MatCardContent,
+    ReactiveFormsModule,
+    SelectionSuggestComponent,
+    MatLabel,
+    MatOption,
+    MatSelect,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTable,
+    MatCell,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    DatePipe,
+    DecimalPipe,
+    MatNoDataRow,
+    MatFooterRow,
+    MatFooterRowDef,
+    MatFooterCell,
+    MatFooterCellDef,
+    DataCalculateTotal,
+  ],
+  templateUrl: './hotel-cost-domestic.component.html',
+  styleUrl: './hotel-cost-domestic.component.scss',
+})
+export class HotelCostDomesticComponent
+  extends CommonComponent
+  implements OnInit
+{
+  markets: string[] = [];
+  monthSelection: string[] = [];
+  listYear: number[] = [];
+  fb: FormBuilder = inject(FormBuilder);
+  flightMarketService = inject(FlightMarketService);
+  override baseService = inject(AvesCostTrackingService);
+  displayedFirst: {
+		name: string;
+		field: string;
+		rowSpan: number;
+		colSpan: number;
+		key: string;
+	}[] = [
+      { name: '#', field: '#', rowSpan: 2, colSpan: 1, key: '' },
+      { name: 'checkin', field: 'Check-in', rowSpan: 1, colSpan: 3, key: '' },
+      { name: 'checkout', field: 'Check-out', rowSpan: 1, colSpan: 3, key: '' },
+      { name: 'quantity', field: 'Quantity', rowSpan: 1, colSpan: 3, key: '' },
+      {
+        name: 'standardRoom',
+        field: 'Standard Room',
+        rowSpan: 1,
+        colSpan: 3,
+        key: '',
+      },
+      {
+        name: 'numberOfNights',
+        field: 'Number of Nights',
+        rowSpan: 2,
+        colSpan: 1,
+        key: 'overnightDays',
+      },
+      {
+        name: 'timesStay',
+        field: 'Time Stay',
+        rowSpan: 2,
+        colSpan: 1,
+        key: 'timesStay',
+      },
+      {
+        name: 'earlyCheckin',
+        field: 'Early Check-in',
+        rowSpan: 2,
+        colSpan: 1,
+        key: 'isEarlyCheckin',
+      },
+      {
+        name: 'lateCheckout',
+        field: 'Late Check-out',
+        rowSpan: 2,
+        colSpan: 1,
+        key: 'isLateCheckout',
+      },
+      {
+        name: 'totalNights',
+        field: 'Total Nights',
+        rowSpan: 2,
+        colSpan: 1,
+        key: 'totalNight',
+      },
+      {
+        name: 'roomCharge',
+        field: 'Room Charge',
+        rowSpan: 1,
+        colSpan: 3,
+        key: '',
+      },
+      {
+        name: 'totalChargeVnd',
+        field: 'Total Charge (VND)',
+        rowSpan: 2,
+        colSpan: 1,
+        key: 'totalVndCharge',
+      },
+    ];
+  displayedSecond = [
+    { name: 'fltnoIn', field: 'Arr Flt No', key: 'fltnoIn' },
+    { name: 'checkinDate', field: 'Date', key: 'checkinDate' },
+    { name: 'checkinTime', field: 'Time', key: 'checkinDate' },
+    { name: 'fltnoOut', field: 'Dep Flt No', key: 'fltnoOut' },
+    { name: 'checkoutDate', field: 'Date', key: 'checkoutDate' },
+    { name: 'checkoutTime', field: 'Time', key: 'checkoutDate' },
+    { name: 'quantityfc', field: 'FC', key: 'fcCount' },
+    { name: 'quantityCcMen', key: 'maleCcCount', field: 'CC Men' },
+    { name: 'quantityCcWomen', field: 'CC Women', key: 'femaleCcCount' },
+    { name: 'fcSingle', field: 'FC Single', key: 'fcCount' },
+    { name: 'ccSingle', field: 'CC Single', key: 'ccSingleRooms' },
+    { name: 'ccTwin', field: 'CC Twin', key: 'ccRooms' },
+    { name: 'roomChargeFc', field: 'FC Single', key: 'fcOntimeCharge' },
+    { name: 'roomChargeCc', field: 'CC Single', key: 'ccSingleOntimeCharge' },
+    { name: 'roomChargeTwin', field: 'CC Twin', key: 'ccTwinsOntimeCharge' },
+  ];
+  displayedFirstArr: string[];
+  displayedSecondArr: string[];
+  displayedColumnsArr: string[];
+
+  constructor() {
+    super();
+    for (let i = 1; i <= 12; i++) {
+      this.monthSelection.push(i + '');
+    }
+    const currentYear = new Date().getFullYear();
+    const startYear = Math.floor(currentYear / 100) * 100;
+    const endYear = startYear + 99;
+
+    for (let year = startYear; year <= endYear; year++) {
+      this.listYear.push(year);
+    }
+
+    this.formGroupSearch = this.fb.group({
+      marketCode: [''],
+      month: [''],
+      year: [],
+    });
+
+    this.displayedFirstArr = this.displayedFirst.map((item) => item.name);
+    this.displayedSecondArr = this.displayedSecond.map((item) => item.name);
+    this.displayedColumnsArr = [
+      '#',
+      ...this.displayedSecond.slice(0, 12).map((item) => item.name),
+      ...this.displayedFirst
+        .filter(
+          (item) =>
+            item.rowSpan === 2 &&
+						item.name !== '#' &&
+						item.name !== 'totalChargeVnd',
+        )
+        .map((item) => item.name),
+      ...this.displayedSecond.slice(12).map((item) => item.name),
+      this.displayedFirst[this.displayedFirst.length - 1].name,
+    ];
+  }
+
+  override async ngOnInit() {
+    await this.spinner.show();
+    this.markets = (
+      await this.flightMarketService.search({
+        option: 1,
+        type: 'Domestic',
+        status: 'Operational',
+      })
+    ).data;
+    this.formGroupSearch.patchValue({
+      marketCode: this.markets[0],
+      month: new Date().getMonth() + 1 + '',
+      year: new Date().getFullYear(),
+    });
+    this.dataSource.data = (
+      await this.baseService.search(this.formGroupSearch.getRawValue())
+    ).data;
+    await this.spinner.hide();
+  }
+
+  override async search() {
+    await this.spinner.show();
+    const { marketCode, month, year } = this.formGroupSearch.value;
+    const response = await this.baseService.search({
+      marketCode,
+      month,
+      year,
+    });
+    this.dataSource.data = response.data;
+
+    await this.spinner.hide();
+  }
+
+  async exportDownloadFile() {
+    let exportObj = this.formGroupSearch.getRawValue();
+    exportObj = { ...exportObj, export: true };
+    await this.exportFileOptions(exportObj);
+  }
+}
--- src/app/crew-trip/features/plan/budget-procurement/budget-procurement-common/domestic/domestic-budget-procurement-hotel/domestic-budget-procurement-hotel.component.ts
+++ src/app/crew-trip/features/plan/budget-procurement/budget-procurement-common/domestic/domestic-budget-procurement-hotel/domestic-budget-procurement-hotel.component.ts
@@ -1,32 +1,58 @@
 import { CommonModule, DatePipe } from '@angular/common';
-import { AfterViewChecked, ChangeDetectionStrategy, ChangeDetectorRef, Component, effect, inject, input, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  effect,
+  inject,
+  input,
+} from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule, MatFormField } from '@angular/material/form-field';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { DigitOnlyModule } from '@uiowa/digit-only';
 import { ClickOutside } from 'ngxtension/click-outside';
 import { DataTransformPipe } from 'src/app/crew-trip/shared/data-transform.pipe';
 import { InputSizeComponent } from 'src/app/crew-trip/shared/input/input-size.component';
-import { Constant } from 'src/app/crew-trip/shared/utils/constant';
-import { formula, getHeaderRowDef1, getHeaderRowDef2, getRowDef } from './domestic-budget-procurement-hotel.model';
 import { truncateDateUTC } from 'src/app/crew-trip/shared/utils/common';
+import { Constant } from 'src/app/crew-trip/shared/utils/constant';
 import { PADDING_0, PlanCategoryEnum } from '../../../budget-procurement.model';
-import { co } from 'node_modules/@fullcalendar/core/internal-common';
+import {
+  formula,
+  getHeaderRowDef1,
+  getHeaderRowDef2,
+  getRowDef,
+} from './domestic-budget-procurement-hotel.model';
 
 @Component({
   selector: 'app-domestic-budget-procurement-hotel',
   standalone: true,
-  imports: [MatTableModule, CommonModule, MatFormFieldModule, MatFormField, MatInputModule, InputSizeComponent,
-    FormsModule, ReactiveFormsModule, ClickOutside, MatButtonModule, DataTransformPipe, DigitOnlyModule],
+  imports: [
+    MatTableModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatFormField,
+    MatInputModule,
+    InputSizeComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    ClickOutside,
+    MatButtonModule,
+    DataTransformPipe,
+    DigitOnlyModule,
+  ],
   templateUrl: './domestic-budget-procurement-hotel.component.html',
   styleUrl: './domestic-budget-procurement-hotel.component.scss',
   providers: [DatePipe, DataTransformPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DomesticBudgetProcurementHotelComponent implements OnInit, AfterViewChecked {
-
+export class DomesticBudgetProcurementHotelComponent
+implements OnInit, AfterViewChecked
+{
   yearPlan = input<number>(2024); // năm kế hoạch
   updateBudgetPlan = input<boolean | undefined>(false); //tích chọn check box Lập kế hoạch sản lượng thay đổi
   type = input<PlanCategoryEnum>(PlanCategoryEnum.BUDGET); // Loại Ngân sách hoặc mua sắm (budget/procurement)
@@ -43,21 +69,28 @@ export class DomesticBudgetProcurementHotelComponent implements OnInit, AfterVie
   PADDING_0 = PADDING_0;
   PlanCategoryEnum = PlanCategoryEnum;
 
-  constructor(private datePipe: DatePipe, private cdRef: ChangeDetectorRef) {
+  constructor(
+		private datePipe: DatePipe,
+		private cdRef: ChangeDetectorRef,
+  ) {
     effect(() => {
-      console.log('effect data DomesticBudgetProcurementHotelComponent: ', this.data())
+      console.log(
+        'effect data DomesticBudgetProcurementHotelComponent: ',
+        this.data(),
+      );
       if (this.data()) {
         this.setDataSource(this.data());
       }
-    })
+    });
   }
+
   ngOnInit(): void {
     this.getRow();
   }
+
   ngAfterViewChecked(): void {
     this.cdRef.detectChanges(); // Phát hiện và cập nhật các thay đổi
   }
-
 
   setDataSource(data: any[]) {
     this.dataSource.data = [...data];
@@ -73,7 +106,7 @@ export class DomesticBudgetProcurementHotelComponent implements OnInit, AfterVie
       item.periodLabel = period;
       this.calculateData(item, index);
     });
-  };
+  }
 
   getRow(): void {
     this.headerRowDef1 = getHeaderRowDef1(null, this.type());
@@ -82,9 +115,9 @@ export class DomesticBudgetProcurementHotelComponent implements OnInit, AfterVie
   }
 
   /**
-   * 
-   * @param item Giá trị từng dòng của dataSource theo công thức
-   */
+	 *
+	 * @param item Giá trị từng dòng của dataSource theo công thức
+	 */
   private calculateData(item: any, index: number) {
     // Tổng Số phòng đơn
     this.calculate(item, 'totalSingleRoom');
@@ -98,7 +131,6 @@ export class DomesticBudgetProcurementHotelComponent implements OnInit, AfterVie
     this.calculate(item, 'totalAmountYearPerformVat');
   }
 
-
   // hàm công thức tính chung
   calculate(item: any, key: string) {
     // let data: any = this.dataSource.data[index];
@@ -111,7 +143,7 @@ export class DomesticBudgetProcurementHotelComponent implements OnInit, AfterVie
         strFomular = objFormula.formulaUpdateBudgetPlan;
       }
     }
-    if (!!strFomular) {
+    if (strFomular) {
       item[key] = this.calculateFormula(item, strFomular);
     }
     return item[key];
@@ -121,42 +153,55 @@ export class DomesticBudgetProcurementHotelComponent implements OnInit, AfterVie
   calculateFormula(data: any, formula: string): number {
     // Sử dụng Function để tạo hàm động từ công thức
     const dynamicFunction = new Function(
-      'data', 'generalData',
-      `return ${formula};`    // Công thức cần tính
+      'data',
+      'generalData',
+      `return ${formula};`, // Công thức cần tính
     );
     const result = dynamicFunction(data, null);
     return Math.round(result);
   }
 
-  // TÍnh dòng tổng 
+  // TÍnh dòng tổng
   getTotal(control: string) {
     //Cột Thành tiền VND - bao gồm VAT:   tính tổng từ T12/2024-T11/2025,   còn các cột còn lại đều tính tổng từ T1/2025-T12/2025
     const startDatePlanGroup = new Date(this.yearPlan(), 0, 1);
     if (control === 'totalAmountVat') {
       const endDatePlanGroup = new Date(this.yearPlan(), 10, 1);
-      return Math.round(this.dataSource.data.map((t: any) => {
-        if (truncateDateUTC(new Date(t['periodStart'])) <= truncateDateUTC(endDatePlanGroup)) {
-          return Number(t[control]);
-        } else {
-          return Number(t['totalAmountYearPerformVat'])
-        }
-      }).reduce((acc, value) => acc + value, 0));
+      return Math.round(
+        this.dataSource.data
+          .map((t: any) => {
+            if (
+              truncateDateUTC(new Date(t['periodStart'])) <=
+							truncateDateUTC(endDatePlanGroup)
+            ) {
+              return Number(t[control]);
+            } else {
+              return Number(t['totalAmountYearPerformVat']);
+            }
+          })
+          .reduce((acc, value) => acc + value, 0),
+      );
     }
-    return Math.round(this.dataSource.data.map((t: any) => {
-      if (truncateDateUTC(new Date(t['periodStart'])) >= truncateDateUTC(startDatePlanGroup)) {
-        return Number(t[control]);
-      }
-      return 0;
-    }).reduce((acc, value) => acc + value, 0));;
+    return Math.round(
+      this.dataSource.data
+        .map((t: any) => {
+          if (
+            truncateDateUTC(new Date(t['periodStart'])) >=
+						truncateDateUTC(startDatePlanGroup)
+          ) {
+            return Number(t[control]);
+          }
+          return 0;
+        })
+        .reduce((acc, value) => acc + value, 0),
+    );
   }
-
-
 
   clickEdit(data: any, control: string) {
     data[control] = true;
   }
+
   clickOutside(data: any, control: string) {
     data[control] = false;
   }
-
 }
