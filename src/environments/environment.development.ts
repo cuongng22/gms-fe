4398e589a3d6bff7ac49b64b45cdd87a7a5926a6
Do Xuan Hop <dxhop@gimasys.com>
2025-01-21 01:58:13 +0000

    Update API URL to localhost for local

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: 'http://192.168.10.68:8081/api',
-  // apiUrl: 'http://127.0.0.1:8081/api',
-  baseUrl: 'http://192.168.10.68:8081',
-  socketUrl: 'ws://192.168.10.68:8081'
+	// apiUrl: 'http://192.168.10.68:8081/api',
+	apiUrl: 'http://127.0.0.1:8081/api',
+	baseUrl: 'http://192.168.10.68:8081',
+	socketUrl: 'ws://192.168.10.68:8081',
 };
