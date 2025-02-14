2b0abf61d03c5bca3b05c27e2907e537632762c1
TheHoa <thehoa268@gmail.com>
2025-02-14 16:43:03 +0700

    fix bug group mail

--- src/app/crew-trip/core/auth/auth.interceptor.ts
+++ src/app/crew-trip/core/auth/auth.interceptor.ts
@@ -83,7 +83,6 @@ export function loggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerF
         } else if (errorResponse.status === HttpStatusCode.Conflict ||
           errorResponse.status === HttpStatusCode.NotFound) {
         } else {
-          console.log("errorResponse", errorResponse)
           baseService.showError(errorResponse?.error?.error ?? MESSAGE.ERROR);
         }
       });
