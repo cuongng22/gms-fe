f1971c438558c772e237a1086b88582eff362a05
dxhop <dxhop@gimasys.com>
2025-03-23 14:04:53 +0700

    fix charter

--- src/app/crew-trip/shared/data-calculate-total.ts
+++ src/app/crew-trip/shared/data-calculate-total.ts
@@ -1,21 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dataCalculateTotal',
-  standalone: true,
+	name: 'dataCalculateTotal',
+	standalone: true,
 })
 export class DataCalculateTotal implements PipeTransform {
-  transform(item: any[], type: string): number {
-    let total = 0;
-    return item.map(i => i[type]).reduce((preValue, currValue) => Number(this.ctz(preValue)) + Number(this.ctz(currValue)), 0)
-  }
+	transform(item: any[], type: string): number {
+		return item
+			.map((i) => i[type])
+			.reduce(
+				(preValue, currValue) =>
+					Number(this.ctz(preValue)) + Number(this.ctz(currValue)),
+				0,
+			);
+	}
 
-  /**
-   * Convert to Zero
-   * @param value 
-   * @returns 
-   */
-  ctz(value: any) {
-    return value ?? 0
-  }
+	/**
+	 * Convert to Zero
+	 * @param value
+	 * @returns
+	 */
+	ctz(value: any) {
+		return value ?? 0;
+	}
 }
