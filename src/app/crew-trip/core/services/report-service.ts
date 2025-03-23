4fdb05b22d8689cf87de96a7141e6baa206062ee
TheHoa <thehoa268@gmail.com>
2025-03-23 10:22:22 +0700

    fix loi

--- src/app/crew-trip/core/services/report-service.ts
+++ src/app/crew-trip/core/services/report-service.ts
@@ -1,6 +1,8 @@
 import {Injectable, model} from '@angular/core';
 import {firstValueFrom, Observable, of} from 'rxjs';
 import {BaseService} from 'src/app/crew-trip/core/services/base-service';
+import {HttpParams} from "@angular/common/http";
+import {removeNullValues} from "src/app/crew-trip/shared/utils/constant";
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,10 @@ export class ReportService extends BaseService {
   constructor() {
     super();
     this.path = 'report';
+  }
+
+  getReportLink(reportCode: string) {
+    const url = `${this.api}/${this.path}/viewName=${reportCode}`;
+    return firstValueFrom(this.http.get<any>(url));
   }
 }
