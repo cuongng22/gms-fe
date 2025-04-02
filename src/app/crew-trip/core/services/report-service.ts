bd76e457f6458af8502986277da3a02289435d8a
thehoa <othoa@gimasys.com>
2025-04-03 00:30:51 +0700

    commit fix nhúng tableau

--- src/app/crew-trip/core/services/report-service.ts
+++ src/app/crew-trip/core/services/report-service.ts
@@ -1,8 +1,6 @@
 import {Injectable, model} from '@angular/core';
 import {firstValueFrom, Observable, of} from 'rxjs';
 import {BaseService} from 'src/app/crew-trip/core/services/base-service';
-import {HttpParams} from "@angular/common/http";
-import {removeNullValues} from "src/app/crew-trip/shared/utils/constant";
 
 @Injectable({
   providedIn: 'root'
--- src/app/crew-trip/features/reports/report1/report1.component.ts
+++ src/app/crew-trip/features/reports/report1/report1.component.ts
@@ -1,7 +1,7 @@
 import {
-  Component,
+  Component, CUSTOM_ELEMENTS_SCHEMA,
   inject,
-  OnInit,
+  OnInit
 } from '@angular/core';
 import {CommonComponent} from 'src/app/crew-trip/shared/common.component';
 import {CommonModule, NgClass, NgIf, TitleCasePipe} from '@angular/common';
@@ -24,55 +24,88 @@ import {MatAutocompleteModule, MatAutocompleteTrigger} from '@angular/material/a
 import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
 import {Constant, DATE_FORMAT_DD_MM_YYYY} from 'src/app/crew-trip/shared/utils/constant';
 import {MAT_MOMENT_DATE_FORMATS, provideMomentDateAdapter} from '@angular/material-moment-adapter';
-import {ReportService} from "src/app/crew-trip/core/services/report-service";
-import {HttpStatusCode} from "@angular/common/http";
+import {ReportService} from 'src/app/crew-trip/core/services/report-service';
+import { TableauModule } from 'ngx-tableau';
+
 
 @Component({
-  selector: 'app-report',
-  standalone: true,
-  imports: [MatCardModule, FormsModule, MatFormFieldModule, ReactiveFormsModule, MatSelectModule, MatButtonModule,
-    MatFormField, MatInputModule, InputSizeComponent, MatDatepickerModule,
-    MatNativeDateModule, NgxMaterialTimepickerModule, MatAutocompleteModule, CommonModule,
-    MatTableModule, MatPaginatorModule
-  ],
-  providers: [DataTransformPipe,
-    provideMomentDateAdapter(DATE_FORMAT_DD_MM_YYYY),
-  ],
-  templateUrl: './report1.component.html',
-  styleUrl: './report1.component.scss'
+	selector: 'app-report',
+	standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	imports: [
+		MatCardModule,
+		FormsModule,
+		MatFormFieldModule,
+		ReactiveFormsModule,
+		MatSelectModule,
+		MatButtonModule,
+		MatFormField,
+		MatInputModule,
+		InputSizeComponent,
+		MatDatepickerModule,
+		MatNativeDateModule,
+		NgxMaterialTimepickerModule,
+		MatAutocompleteModule,
+		CommonModule,
+		MatTableModule,
+		MatPaginatorModule,
+		TableauModule,
+	],
+	providers: [
+		DataTransformPipe,
+		provideMomentDateAdapter(DATE_FORMAT_DD_MM_YYYY),
+	],
+	templateUrl: './report1.component.html',
+	styleUrl: './report1.component.scss',
 })
 export class reportcomponent extends CommonComponent implements OnInit {
-  override baseService = inject(ReportService);
-  iframeUrl: SafeResourceUrl;
-  codeReport: string = "BC_7_2";
+	override baseService = inject(ReportService);
+	iframeUrl = '';
+  serverUrl: string;
+  ticket: string;
+  report: string;
+	codeReport = 'BC_7_2';
 
-  constructor(private sanitizer: DomSanitizer) {
-    super();
-  }
+	constructor(private sanitizer: DomSanitizer) {
+		super();
+	}
 
-  override async ngOnInit() {
-    await this.spinner.show();
-    try {
-      await this.loadReport()
-    } catch (error: any) {
-      this.showError(error);
-    }
-    await this.spinner.hide();
-  }
+	override async ngOnInit() {
+		await this.spinner.show();
+		try {
+			await this.loadReport();
+		} catch (error: any) {
+			this.showError(error);
+		}
+		await this.spinner.hide();
+	}
 
-  async loadReport() {
-    try {
-      this.baseService.getReportLink(this.codeReport).then(res => {
-        console.log("rsssss:",res.data)
-        this.iframeUrl = this.sanitizeUrl(res.data);
-        // this.iframeUrl = this.sanitizeUrl('https://crewtripreport.vietnamairlines.com/trusted/je6uoh7qTn6zVWHiwfxqVA==:G6J_26cjbpA8W5Gb93Hwcs64/views/BC_7_2/BC_7_2');
-      });
-    } catch (Error: any) {
-      console.log(Error);
-    }
-  }
+	async loadReport() {
+		try {
+			this.baseService.getReportLink(this.codeReport).then((res) => {
+				this.iframeUrl = res.data;
+        // const serverEndIndex = this.iframeUrl.indexOf('/trusted/');
+        // this.serverUrl = this.iframeUrl.substring(0, serverEndIndex);
+        //
+        // const ticketStartIndex = serverEndIndex + '/trusted/'.length;
+        // const ticketEndIndex = this.iframeUrl.indexOf('/views/');
+        // this.ticket = this.iframeUrl.substring(ticketStartIndex, ticketEndIndex);
+        //
+        // const reportStartIndex = ticketEndIndex + 1;
+        // const queryIndex = this.iframeUrl.indexOf('?');
+        // this.report = queryIndex === -1 ? this.iframeUrl.substring(reportStartIndex) : this.iframeUrl.substring(reportStartIndex, queryIndex);
+        // // Log để kiểm tra giá trị
+        // console.log('Server URL:', this.serverUrl);
+        // console.log('Ticket:', this.ticket);
+        // console.log('Report:', this.report);
+				// this.iframeUrl = this.sanitizeUrl('https://crewtripreport.vietnamairlines.com/trusted/je6uoh7qTn6zVWHiwfxqVA==:G6J_26cjbpA8W5Gb93Hwcs64/views/BC_7_2/BC_7_2');
+			});
+		} catch (Error: any) {
+			console.log(Error);
+		}
+	}
 
-  sanitizeUrl(url: string): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
+	sanitizeUrl(url: string): SafeResourceUrl {
+		return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+	}
 }
