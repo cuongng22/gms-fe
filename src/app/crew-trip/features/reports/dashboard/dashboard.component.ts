649c0959743a76237503c9261c1ccd41b4cae8d7
thehoa <othoa@gimasys.com>
2025-04-04 16:16:20 +0700

    update report

--- src/app/crew-trip/features/reports/dashboard/dashboard.component.ts
+++ src/app/crew-trip/features/reports/dashboard/dashboard.component.ts
@@ -1,13 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatCard, MatCardContent, MatCardModule } from '@angular/material/card';
 import { CommonComponent } from 'src/app/crew-trip/shared/common.component';
 import { ReportService } from 'src/app/crew-trip/core/services/report-service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { InputSizeComponent } from 'src/app/crew-trip/shared/input/input-size.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
 	selector: 'app-dashboard',
 	standalone: true,
-	imports: [MatCard, MatCardContent],
+  imports: [MatCardModule, FormsModule, MatFormFieldModule, ReactiveFormsModule, MatSelectModule, MatButtonModule,
+    MatFormField, MatInputModule, InputSizeComponent, MatDatepickerModule,
+    MatNativeDateModule, NgxMaterialTimepickerModule, MatAutocompleteModule, CommonModule,
+    MatTableModule, MatPaginatorModule
+  ],
 	templateUrl: './dashboard.component.html',
 	styleUrl: './dashboard.component.scss',
 })
