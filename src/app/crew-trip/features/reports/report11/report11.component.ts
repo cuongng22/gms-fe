0a357a217e49166728f2aac2b2e527a6496b2722
thehoa <othoa@gimasys.com>
2025-04-04 14:39:52 +0700

    update report

---
+++ src/app/crew-trip/features/reports/report11/report11.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report11',
+  standalone: true,
+  imports: [],
+  templateUrl: './report11.component.html',
+  styleUrl: './report11.component.scss'
+})
+export class Report11Component {
+
+}
