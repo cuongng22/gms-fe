6f68168d427467eae8ecac03075e6289a859f1b4
Do Xuan Hop <dxhop@gimasys.com>
2025-04-14 11:43:29 +0000

    Feature/develop

--- src/app/crew-trip/features/flight-schedules/daily-flight-schedules/monthly-flight-schedule/other-flight-schedule/other-flight-schedule.component.ts
+++ src/app/crew-trip/features/flight-schedules/daily-flight-schedules/monthly-flight-schedule/other-flight-schedule/other-flight-schedule.component.ts
@@ -68,7 +68,7 @@ export class OtherFlightScheduleComponent extends CommonComponent {
   async onSearch(event: any) {
     const response = await this.search(event, false, this.baseService.searchExtraCrews.bind(this.baseService));
     if (response.data.content && response.data.content.length > 0) {
-      const result = response.data.map((item: any) => {
+      const result = response.data.content.map((item: any) => {
         const _checkinDate = moment(item.FLIGHT_DATE_IN.split(" ")[0], 'DD/MM/YYYY')
         // const _checkoutDate = new Date(item.FLIGHT_DATE_OUT.split(" ")[0])
         const _currDate = moment()
