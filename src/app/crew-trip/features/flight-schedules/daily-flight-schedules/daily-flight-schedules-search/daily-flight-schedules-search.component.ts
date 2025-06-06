5b7df4c3d793a6925e6c638dda01ac4799811780
Do Xuan Hop <dxhop@gimasys.com>
2025-06-06 04:42:36 +0000

    refactor: improve email supplier and flight schedules components for better...

--- src/app/crew-trip/features/flight-schedules/daily-flight-schedules/daily-flight-schedules-search/daily-flight-schedules-search.component.ts
+++ src/app/crew-trip/features/flight-schedules/daily-flight-schedules/daily-flight-schedules-search/daily-flight-schedules-search.component.ts
@@ -1,90 +1,116 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit, output, viewChild } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import {
+	FormBuilder,
+	FormsModule,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatFormFieldModule, MatFormField } from '@angular/material/form-field';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { SelectionSuggestComponent } from 'src/app/crew-trip/shared/component/selection-suggest/selection-suggest.component';
-import { InputSizeComponent } from 'src/app/crew-trip/shared/input/input-size.component';
-import { getYear, months } from '../daily-flight-schedules.model';
 import { FlightMarketService } from 'src/app/crew-trip/core/services/flight-market.service';
-import { FlightMarketStatusEnum } from '../../../category/flight-market/flight-market.model';
-import { ListResponse } from 'src/app/crew-trip/shared/models/common.model';
+import { SelectionSuggestComponent } from 'src/app/crew-trip/shared/component/selection-suggest/selection-suggest.component';
 import { SelectionComponent } from 'src/app/crew-trip/shared/component/selection/selection.component';
+import { InputSizeComponent } from 'src/app/crew-trip/shared/input/input-size.component';
+import { FlightMarketStatusEnum } from '../../../category/flight-market/flight-market.model';
+import { getYear, months } from '../daily-flight-schedules.model';
 
 @Component({
-  selector: 'app-daily-flight-schedules-search',
-  standalone: true,
-  imports: [
-    MatCardModule, FormsModule, MatFormFieldModule, ReactiveFormsModule, MatSelectModule, MatButtonModule,
-    MatFormField, MatInputModule, InputSizeComponent,
-    MatNativeDateModule, MatAutocompleteModule, CommonModule,
-    SelectionSuggestComponent, SelectionComponent
-  ],
-  templateUrl: './daily-flight-schedules-search.component.html',
-  styleUrl: './daily-flight-schedules-search.component.scss'
+	selector: 'app-daily-flight-schedules-search',
+	standalone: true,
+	imports: [
+		MatCardModule,
+		FormsModule,
+		MatFormFieldModule,
+		ReactiveFormsModule,
+		MatSelectModule,
+		MatButtonModule,
+		MatFormField,
+		MatInputModule,
+		InputSizeComponent,
+		MatNativeDateModule,
+		MatAutocompleteModule,
+		CommonModule,
+		SelectionSuggestComponent,
+		SelectionComponent,
+	],
+	templateUrl: './daily-flight-schedules-search.component.html',
+	styleUrl: './daily-flight-schedules-search.component.scss',
 })
 export class DailyFlightSchedulesSearchComponent implements OnInit {
-  flightMarketService = inject(FlightMarketService)
-  search = output<any>();
-  formBuilder = inject(FormBuilder);
+	flightMarketService = inject(FlightMarketService);
+	search = output<any>();
+	formBuilder = inject(FormBuilder);
 
-  airports: any[] = [];
-  months: any[] = []
-  years: any[] = []
+	airports: any[] = [];
+	months: any[] = [];
+	years: any[] = [];
 
+	formGroupSearch = this.formBuilder.group({
+		airport: ['', Validators.required],
+		month: '',
+		year: 0,
+		timeZone: '',
+	});
 
-  formGroupSearch = this.formBuilder.group({
-    airport: ['', Validators.required],
-    month: '',
-    year: 0,
-    timeZone: ''
-  });
+	async ngOnInit() {
+		const airportResponse = await this.flightMarketService.search<any>({
+			option: 0,
+			page: 0,
+			size: 999999,
+			limit: 999999,
+			status: FlightMarketStatusEnum.OPERATIONAL,
+		});
+		this.airports = [
+			...airportResponse.data.content.map((item: any) => {
+				return {
+					marketCode: item.marketCode,
+					marketName: item.marketName,
+					timezone: item.timezone,
+				};
+			}),
+		];
+		this.months = months;
+		this.years = getYear();
 
-  async ngOnInit() {
-    const airportResponse = await this.flightMarketService.search<any>({ option: 0, page: 0, size: 999999, limit: 999999, status: FlightMarketStatusEnum.OPERATIONAL });
-    this.airports = [...airportResponse.data.content.map((item: any) => {
-      return {
-        marketCode: item.marketCode,
-        marketName: item.marketName,
-        timezone: item.timezone
-      }
-    })];
-    this.months = months;
-    this.years = getYear();
+		this.formGroupSearch.controls.airport.setValue(this.airports[0].marketCode);
+		this.formGroupSearch.controls.timeZone.setValue(this.airports[0].timezone);
 
-    this.formGroupSearch.controls.airport.setValue(this.airports[0].marketCode);
-    this.formGroupSearch.controls.timeZone.setValue(this.airports[0].timezone);
+		const currentMonth = new Date().getMonth() + 1;
+		this.formGroupSearch.controls.month.setValue(
+			currentMonth < 10
+				? '0' + currentMonth.toString()
+				: currentMonth.toString(),
+		);
+		this.formGroupSearch.controls.year.setValue(new Date().getFullYear());
 
-    const currentMonth = new Date().getMonth() + 1;
-    this.formGroupSearch.controls.month.setValue(currentMonth < 10 ? '0' + currentMonth.toString() : currentMonth.toString());
-    this.formGroupSearch.controls.year.setValue(new Date().getFullYear());
+		this.onSearch();
+	}
 
-    this.onSearch()
+	airportChange(value: any) {
+		const airportFilter = this.airports.find(
+			(item) => item.marketCode === value.value,
+		);
+		this.formGroupSearch.controls.timeZone.setValue(airportFilter?.timezone);
+	}
 
-  }
-
-  airportChange(value: any) {
-    const airportFilter = this.airports.find(item => item.marketCode === value.value);
-    this.formGroupSearch.controls.timeZone.setValue(airportFilter?.timezone);
-  }
-
-
-  onSearch() {
-    this.formGroupSearch.markAllAsTouched();
-    if (this.formGroupSearch.invalid) {
-      return;
-    }
-    const bodySearch = {
-      ...this.formGroupSearch.value,
-      timezone: this.airports.find((item: any) => item.marketCode === this.formGroupSearch.value.airport)?.timezone
-    }
-    console.log(bodySearch)
-    this.search.emit(this.formGroupSearch.value);
-  }
-
+	onSearch() {
+		this.formGroupSearch.markAllAsTouched();
+		if (this.formGroupSearch.invalid) {
+			return;
+		}
+		const bodySearch = {
+			...this.formGroupSearch.value,
+			timezone: this.airports.find(
+				(item: any) => item.marketCode === this.formGroupSearch.value.airport,
+			)?.timezone,
+		};
+		console.log(bodySearch);
+		this.search.emit(this.formGroupSearch.value);
+	}
 }
--- src/app/crew-trip/shared/utils/constant.ts
+++ src/app/crew-trip/shared/utils/constant.ts
@@ -2,199 +2,211 @@ import { effect, Signal, signal } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 
 export class Constant {
-
-  static DATE_FORMAT = 'DD/MM/YYYY';
-  static DATE_TIME_FORMAT = 'DD/MM/YYYY HH:mm';
-  static MONTH_FORMAT = 'MM/YYYY';
-  static TIME_FORMAT = 'HH:mm';
-  static NUMBER = 'number';
-  static DATE = 'date';
-  static STRING_FORMAT = 'string_format';
-  static PAGE = 0;
-  static PAGE_SIZE = 20;
-  static TIME_ZONE = 'Asia/Ho_Chi_Minh';
-  static DATE_FORMAT_YYYYMMDD = 'YYYY/MM/DD';
-  static LOCAL_DATE_FORMAT = 'YYYY-MM-DD'
-  static LOCAL_DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+	static DATE_FORMAT = 'DD/MM/YYYY';
+	static DATE_TIME_FORMAT = 'DD/MM/YYYY HH:mm';
+	static MONTH_FORMAT = 'MM/YYYY';
+	static TIME_FORMAT = 'HH:mm';
+	static NUMBER = 'number';
+	static DATE = 'date';
+	static STRING_FORMAT = 'string_format';
+	static PAGE = 0;
+	static PAGE_SIZE = 20;
+	static TIME_ZONE = 'Asia/Ho_Chi_Minh';
+	static DATE_FORMAT_YYYYMMDD = 'YYYY/MM/DD';
+	static LOCAL_DATE_FORMAT = 'YYYY-MM-DD';
+	static LOCAL_DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 }
 
 export class MESSAGE {
-  static ERROR = $localize`:@@error:Something wrong`;
-  static ERROR_CONNECT = $localize`:@@errorConnect:Unable to connect to server`;
-  static CREATE_SUCCESS = $localize`:@@createSuccess:Created successfully`;
-  static CREATE_FAIL = $localize`:@@createFail:Create fail`;
-  static UPDATE_SUCCESS = $localize`:@@updateSuccess:Updated successfully`;
-  static UPDATE_FAIL = $localize`:@@updateFail:Update fail`;
-  static DATA_EMPTY = $localize`:@@noDataFound:No data found`;
-  static DELETE_SUCCESS = $localize`:@@deletedSuccessfully:Deleted successfully`;
-  static DELETE_FAIL = $localize`:@@deleteFail:Delete fail`;
-  static UPLOAD_SUCCESS = $localize`:@@importedSuccessfully:Imported successfully`;
-  static REJECT_SUCCESS = $localize`:@@rejectSuccess:Reject successfully`;
-  static REJECT_FAIL = $localize`:@@rejectFail:Reject fail`;
-  static REQUIRED = $localize`:@@required:is required`;
-  static FORMAT_INVALID = $localize`:@@formatInvalid:invalid format.`;
-  static MIN_LENGTH_INVALID = $localize`:@@minLengthInvalid:requires a minimum of %d characters`;
-  static MAX_LENGTH_INVALID = $localize`:@@maxLengthInvalid:must be less than %d characters`;
-  static MUST_LENGTH_INVALID = $localize`:@@mustLengthInvalid:must be %d characters`;
-  static PASSWORD_FORMAT_INVALID = $localize`:@@passwordFormat:must include number, letter and special character.`;
-  static ALREADY_EXISTS = $localize`:@@alreadyExists:already exists`;
-  static ACTIVE = $localize`:@@active:Active`;
-  static INACTIVE = $localize`:@@inactive:Inactive`;
-  static ITEMS_PER_PAGE = $localize`:@@itemsPerPage:Items per page`;
-  static NEXT_PAGE = $localize`:@@nextPage:Next page`;
-  static PRIVIOUS_PAGE = $localize`:@@priviousPage:Privious page`;
-  static MAX_FILE_SIZE = $localize`Invalid file`;
-  static SEND_EMAIL = $localize`Send email successfully`;
-  static FILE_UPLOAD_EMPTY = $localize`The file must not be empty`;
-  static FILE_UPLOAD_INVALID = $localize`The file import is not valid`;
-  static FILE_UPLOAD_INVALID_XLSX = $localize`File type must be .xlsx`;
-  static SUCCESS = $localize`:@@success:Success`;
-  static FAILURE = $localize`:@@failure:Failure`;
+	static ERROR = $localize`:@@error:Something wrong`;
+	static ERROR_CONNECT = $localize`:@@errorConnect:Unable to connect to server`;
+	static CREATE_SUCCESS = $localize`:@@createSuccess:Created successfully`;
+	static CREATE_FAIL = $localize`:@@createFail:Create fail`;
+	static UPDATE_SUCCESS = $localize`:@@updateSuccess:Updated successfully`;
+	static UPDATE_FAIL = $localize`:@@updateFail:Update fail`;
+	static DATA_EMPTY = $localize`:@@noDataFound:No data found`;
+	static DELETE_SUCCESS = $localize`:@@deletedSuccessfully:Deleted successfully`;
+	static DELETE_FAIL = $localize`:@@deleteFail:Delete fail`;
+	static UPLOAD_SUCCESS = $localize`:@@importedSuccessfully:Imported successfully`;
+	static REJECT_SUCCESS = $localize`:@@rejectSuccess:Reject successfully`;
+	static REJECT_FAIL = $localize`:@@rejectFail:Reject fail`;
+	static REQUIRED = $localize`:@@required:is required`;
+	static FORMAT_INVALID = $localize`:@@formatInvalid:invalid format.`;
+	static MIN_LENGTH_INVALID = $localize`:@@minLengthInvalid:requires a minimum of %d characters`;
+	static MAX_LENGTH_INVALID = $localize`:@@maxLengthInvalid:must be less than %d characters`;
+	static MUST_LENGTH_INVALID = $localize`:@@mustLengthInvalid:must be %d characters`;
+	static PASSWORD_FORMAT_INVALID = $localize`:@@passwordFormat:must include number, letter and special character.`;
+	static ALREADY_EXISTS = $localize`:@@alreadyExists:already exists`;
+	static ACTIVE = $localize`:@@active:Active`;
+	static INACTIVE = $localize`:@@inactive:Inactive`;
+	static ITEMS_PER_PAGE = $localize`:@@itemsPerPage:Items per page`;
+	static NEXT_PAGE = $localize`:@@nextPage:Next page`;
+	static PRIVIOUS_PAGE = $localize`:@@priviousPage:Privious page`;
+	static MAX_FILE_SIZE = $localize`Invalid file`;
+	static SEND_EMAIL = $localize`Send email successfully`;
+	static FILE_UPLOAD_EMPTY = $localize`The file must not be empty`;
+	static FILE_UPLOAD_INVALID = $localize`The file import is not valid`;
+	static FILE_UPLOAD_INVALID_XLSX = $localize`File type must be .xlsx`;
+	static SUCCESS = $localize`:@@success:Success`;
+	static FAILURE = $localize`:@@failure:Failure`;
 
-  static LABEL_START_DATE = $localize`:@@startDate:Start Date`;
-  static MESSAGE_START_DATE_REQUIRED = $localize`:@@startDateRequired:Start Date is required`;
+	static LABEL_START_DATE = $localize`:@@startDate:Start Date`;
+	static MESSAGE_START_DATE_REQUIRED = $localize`:@@startDateRequired:Start Date is required`;
 
-  static LABEL_END_DATE = $localize`:@@endDate:End Date`;
-  static MESSAGE_END_DATE_REQUIRED = $localize`:@@endDateRequired:End Date is required`;
+	static LABEL_END_DATE = $localize`:@@endDate:End Date`;
+	static MESSAGE_END_DATE_REQUIRED = $localize`:@@endDateRequired:End Date is required`;
 
-  static END_DATE_LESS_THAN_START_DATE = $localize`:@@endDateLessThanStartDate:End Date must be greater than Start Date`;
+	static END_DATE_LESS_THAN_START_DATE = $localize`:@@endDateLessThanStartDate:End Date must be greater than Start Date`;
 
-  static LABEL_ESTIMATED_TIME = $localize`:@@estimatedTime:Estimated Time`;
-  static MESSAGE_ESTIMATED_TIME_REQUIRED = $localize`:@@estimatedTimeRequired:Estimated Time is required`;
+	static LABEL_ESTIMATED_TIME = $localize`:@@estimatedTime:Estimated Time`;
+	static MESSAGE_ESTIMATED_TIME_REQUIRED = $localize`:@@estimatedTimeRequired:Estimated Time is required`;
 
-  static PHONE_NUMBER_INVALID = $localize`:@@phoneNumberFormatInvalid:Phone number must be numeric and the characters + ( )`;
+	static PHONE_NUMBER_INVALID = $localize`:@@phoneNumberFormatInvalid:Phone number must be numeric and the characters + ( )`;
 
-  static HOTEL_CANNOT_BE_DELETED = $localize`:@@hotelCannotBeDeleted:Hotel cannot be deleted. It is being used in a contract.`;
-  static CAR_COMPANY_CANNOT_BE_DELETED = $localize`:@@carCompanyCannotBeDeleted:Car company cannot be deleted. It is being used in a contract.`;
+	static HOTEL_CANNOT_BE_DELETED = $localize`:@@hotelCannotBeDeleted:Hotel cannot be deleted. It is being used in a contract.`;
+	static CAR_COMPANY_CANNOT_BE_DELETED = $localize`:@@carCompanyCannotBeDeleted:Car company cannot be deleted. It is being used in a contract.`;
 
-  static SYNC_SUCCESS = $localize`:@@synchronizeSuccessfully:Synchronize successfully`;
-  static EXPORT_SUCCESS = $localize`:@@exportSuccessfully:Export successfully`;
+	static SYNC_SUCCESS = $localize`:@@synchronizeSuccessfully:Synchronize successfully`;
+	static EXPORT_SUCCESS = $localize`:@@exportSuccessfully:Export successfully`;
 }
 
 export class LOCALE {
-  static VN = 'vi-VN';
-  static EN = 'en-US';
+	static VN = 'vi-VN';
+	static EN = 'en-US';
 }
 
 export function removeNullValues(obj: any): any {
-  for (const key in obj) {
-    if (obj[key] === null || obj[key] === undefined || obj[key] === '') {
-      delete obj[key];
-    }
-  }
-  return obj;
+	for (const key in obj) {
+		if (obj[key] === null || obj[key] === undefined || obj[key] === '') {
+			delete obj[key];
+		}
+	}
+	return obj;
 }
 
 export function debouncedSignal<T>(input: Signal<T>, timeOutMs = 0): Signal<T> {
-  const debounceSignal = signal(input());
-  effect(() => {
-    const value = input();
-    const timeout = setTimeout(() => {
-      debounceSignal.set(value);
-    }, timeOutMs);
-    return () => {
-      clearTimeout(timeout);
-    };
-  });
-  return debounceSignal;
+	const debounceSignal = signal(input());
+	effect(() => {
+		const value = input();
+		const timeout = setTimeout(() => {
+			debounceSignal.set(value);
+		}, timeOutMs);
+		return () => {
+			clearTimeout(timeout);
+		};
+	});
+	return debounceSignal;
 }
 
 export function decodeToken(token: string): any {
-  try {
-    return jwtDecode(token);
-  } catch (error) {
-    return null;
-  }
+	try {
+		return jwtDecode(token);
+	} catch (error) {
+		return null;
+	}
 }
 
 export const DATE_FORMAT_DD_MM_YYYY = {
-  parse: {
-    dateInput: 'DD/MM/YYYY',
-  },
-  useValue: { useUtc: true },
+	parse: {
+		dateInput: 'DD/MM/YYYY',
+	},
+	useValue: { useUtc: true },
 
-  display: {
-    dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MMMM YYYY',
-    dateA11yLabel: 'DD/MM/YYYY',
-    monthYearA11yLabel: 'MMMM YYYY'
-
-  },
-
+	display: {
+		dateInput: 'DD/MM/YYYY',
+		monthYearLabel: 'MMMM YYYY',
+		dateA11yLabel: 'DD/MM/YYYY',
+		monthYearA11yLabel: 'MMMM YYYY',
+	},
 };
 
 export class COMMON_CONFIG {
-  static TIMEOUT = 20000;
-  static MAX_FILE_SIZE = '5MB';
-  static FILE_ACCEPT = '.doc,.docx,.pdf,.xls,.xlsx';
-  static FILE_ACCEPT_EXCEL = '.xls,.xlsx';
+	static TIMEOUT = 20000;
+	static MAX_FILE_SIZE = '5MB';
+	static FILE_ACCEPT = '.doc,.docx,.pdf,.xls,.xlsx';
+	static FILE_ACCEPT_EXCEL = '.xls,.xlsx';
 }
 
 export class PATTERN {
-  // static PHONE = '^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$' //so () -
-  static PHONE = '^[0-9()+]+$'
-  static STRING_NUMBER = '^[a-zA-Z0-9]+$' //chu va so
-  static EMAIL = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'
-  static EMAIL_MULTI = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(;\\s*[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})*$';
-  static HOUR24 = '^([01]\\d|2[0-3]):([0-5]\\d)$'
-  static STRING = '^[a-zA-Z]+$' //chu
-  static NUMBER = '^[0-9]+$' //so
-  static NUMBER1 = '^[0-9/.]+$' //so '/'
-  static NUMBER2 = '^[0-9]+(\\.[0-9]+)?$';//so thap phan
-  static NUMBER3 = '^-?[0-9]+(\\.[0-9]+)?$'; // cho phép số âm và số thập phân
-  static STRING_NUMBER1 = '^[a-zA-Z0-9-.]+$' //chu va so . -
-  static STRING_NUMBER2 = '^[a-zA-Z0-9-./]+$' //chu va so . -
+	// static PHONE = '^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$' //so () -
+	static PHONE = '^[0-9()+]+$';
+	static STRING_NUMBER = '^[a-zA-Z0-9]+$'; //chu va so
+	static EMAIL = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$';
+	static EMAIL_MULTI =
+		'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(;\\s*[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})*$';
+	static HOUR24 = '^([01]\\d|2[0-3]):([0-5]\\d)$';
+	static STRING = '^[a-zA-Z]+$'; //chu
+	static NUMBER = '^[0-9]+$'; //so
+	static NUMBER1 = '^[0-9/.]+$'; //so '/'
+	static NUMBER2 = '^[0-9]+(\\.[0-9]+)?$'; //so thap phan
+	static NUMBER3 = '^-?[0-9]+(\\.[0-9]+)?$'; // cho phép số âm và số thập phân
+	static STRING_NUMBER1 = '^[a-zA-Z0-9-.]+$'; //chu va so . -
+	static STRING_NUMBER2 = '^[a-zA-Z0-9-./]+$'; //chu va so . -
 }
 
 export function round(data: any | any[], fractionDigits?: number) {
-  if (Array.isArray(data) && data.length > 0) {
-    data.forEach(item => {
-      if (typeof (item) === 'number' || typeof (item) === 'string') {
-        if (!isNaN(Number(item))) {
-          const _value = Number(item.toString().replace(/,/g, ''))
-          item = Math.round(fractionDigits ? Number(_value.toFixed(fractionDigits)) : _value);
-        }
-      } else if (typeof (item) === 'object') {
-        Object.keys(item).forEach(key => {
-          if (typeof (item[key]) === 'number' || typeof (item[key]) === 'string') {
-            if (!isNaN(Number(item[key]))) {
-              const _value = Number(item[key].toString().replace(/,/g, ''));
-              item[key] = Math.round(fractionDigits ? Number(_value.toFixed(fractionDigits)) : _value);
-            }
-          }
-        });
-      }
-    });
-    return data;
-  } else if (typeof data === 'object' && data && Object.keys(data).length > 0) {
-    Object.keys(data).forEach(key => {
-      if (typeof (data[key]) === 'number' || typeof (data[key]) === 'string') {
-        if (!isNaN(Number(data[key]))) {
-          const _value = Number(data[key].toString().replace(/,/g, ''));
-          data[key] = Math.round(fractionDigits ? Number(_value.toFixed(fractionDigits)) : _value);
-        }
-      }
-    })
-    return data;
-  } else if ((typeof data === 'number' || typeof data === 'string') && data) {
-    if (!isNaN(Number(data))) {
-      const _value = Number(data.toString().replace(/,/g, ''));
-      return Math.round(fractionDigits ? Number(_value.toFixed(fractionDigits)) : _value);
-    }
-  }
-  return 0
+	if (Array.isArray(data) && data.length > 0) {
+		data.forEach((item) => {
+			if (typeof item === 'number' || typeof item === 'string') {
+				if (!isNaN(Number(item))) {
+					const _value = Number(item.toString().replace(/,/g, ''));
+					item = Math.round(
+						fractionDigits ? Number(_value.toFixed(fractionDigits)) : _value,
+					);
+				}
+			} else if (typeof item === 'object') {
+				Object.keys(item).forEach((key) => {
+					if (typeof item[key] === 'number' || typeof item[key] === 'string') {
+						if (!isNaN(Number(item[key]))) {
+							const _value = Number(item[key].toString().replace(/,/g, ''));
+							item[key] = Math.round(
+								fractionDigits
+									? Number(_value.toFixed(fractionDigits))
+									: _value,
+							);
+						}
+					}
+				});
+			}
+		});
+		return data;
+	} else if (typeof data === 'object' && data && Object.keys(data).length > 0) {
+		Object.keys(data).forEach((key) => {
+			if (typeof data[key] === 'number' || typeof data[key] === 'string') {
+				if (!isNaN(Number(data[key]))) {
+					const _value = Number(data[key].toString().replace(/,/g, ''));
+					data[key] = Math.round(
+						fractionDigits ? Number(_value.toFixed(fractionDigits)) : _value,
+					);
+				}
+			}
+		});
+		return data;
+	} else if ((typeof data === 'number' || typeof data === 'string') && data) {
+		if (!isNaN(Number(data))) {
+			const _value = Number(data.toString().replace(/,/g, ''));
+			return Math.round(
+				fractionDigits ? Number(_value.toFixed(fractionDigits)) : _value,
+			);
+		}
+	}
+	return 0;
 }
-
 
 // convertToZero
 export function ctz(value: any) {
-  if (value) {
-    return new Number(value.toString().replace(',', '.'));
-  }
-  return 0;
+	if (value) {
+		return new Number(value.toString().replace(',', '.'));
+	}
+	return 0;
 }
 
 export class SERVICE_CODE {
-  static TRANSPORT_DAYTIME= "TTDHKT.00167";
-  static TRANSPORT_NIGHTTIME= "TTDHKT.00168";
+	static TRANSPORT_DAYTIME = 'TTDHKT.00167';
+	static TRANSPORT_NIGHTTIME = 'TTDHKT.00168';
 }
+
+export const marketType = {
+	domestic: 'DOMESTIC',
+	international: 'INTERNATIONAL',
+};
