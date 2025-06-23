db281a890ffe7093cb6966316ba92d764ef7d7b6
dxhop <dxhop@gimasys.com>
2025-06-23 10:09:33 +0700

    refactor: enhance date validation logic and improve error handling in aves-config component

--- src/app/crew-trip/features/system/config/aves-config/aves-config.component.ts
+++ src/app/crew-trip/features/system/config/aves-config/aves-config.component.ts
@@ -47,12 +47,39 @@ export class AvesConfigComponent extends CommonComponent {
 			this.formGroupDetail.markAllAsTouched();
 			return;
 		}
-		const data = this.formGroupDetail.value;
+		let data = this.formGroupDetail.value;
 		//validate month and date
 		const month = data.month; //current format = JAN, FEB
-		const date = data.date; //current format = 01, 02, ..., 31
+		let date = data.date; //current format = 01, 02, ..., 31
 		const currentYear = new Date().getFullYear();
-		if (moment(month + date + currentYear, 'MMMDDYYYY').isValid()) {
+		const map = new Map<string, string>();
+		map.set('JAN', '01');
+		map.set('FEB', '02');
+		map.set('MAR', '03');
+		map.set('APR', '04');
+		map.set('MAY', '05');
+		map.set('JUN', '06');
+		map.set('JUL', '07');
+		map.set('AUG', '08');
+		map.set('SEP', '09');
+		map.set('OCT', '10');
+		map.set('NOV', '11');
+		map.set('DEC', '12');
+		if (date < 1 || date > 31) {
+			this.formGroupDetail.controls['date'].setErrors({
+				invalid: true,
+				code: 'INVALID_DATE',
+				message: 'Date must be between 1 and 31.',
+			});
+			return;
+		}
+		if (date < 10 && date.length < 2) {
+			date = '0' + date; // Ensure date is two digits
+			data.date = date; // Update the form control value
+		}
+		if (
+			!moment(map.get(month) + date + currentYear, 'MMDDYYYY', true).isValid()
+		) {
 			this.formGroupDetail.controls['date'].setErrors({
 				invalid: true,
 				code: 'INVALID_DATE_FORMAT',
